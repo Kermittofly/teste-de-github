8cfcc49b1c9bdcd26930abb1849979759006313b
Kermit to fly <gabrielcandidomoreira@gmail.com>
2026-09-17 09:42:09 -0300

    movimento do player e física
    adicionamos movimento do player e física

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y);
+    }
+}
