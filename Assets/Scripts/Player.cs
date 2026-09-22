b598a1f1ca9b38230d4a4b66fcdd1f7f9c7b346b
Kermit to fly <gabrielcandidomoreira@gmail.com>
2026-09-22 09:29:46 -0300

    criação do sistema de pulo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
+    private bool isGrounded = false;
     private Rigidbody2D rb;
     void Start()
     {
@@ -15,10 +17,29 @@ public class Player : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal"); //reconhecer o movimento horizontal
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y);//vai aplicar a velocidade no rigidbody
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); // vai dar força ao pulo
 
         }
     }
+
+    private void OnCollisionEnter2D( Collision2D collision)
+    {
+       if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; //Vai reconhecer quando o jogador estiver no chão
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; //Vai reconhecer quando o jogador não estiver no chão
+        }
+    }
+
+
+
 }
